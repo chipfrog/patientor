f712081fa62b8bde119dc0cff3cc1ef5c542eaad
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2020-10-19 22:53:32 +0300

    9.17

--- src/SinglePatientPage/index.tsx
+++ src/SinglePatientPage/index.tsx
@@ -10,15 +10,19 @@ const PatientPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
 
   React.useState(() => {
-    const fetchPatient = async () => {
-      try {
-        const { data: patientFromApi } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
-        dispatch({ type: "GET_PATIENT", payload: patientFromApi });
-      } catch(error) {
-        console.log(error);
-      }
-    };
-    fetchPatient();
+    const patientToFetch = Object.values(patients).find(p => p.id === id);
+
+    if (patientToFetch !== undefined && patientToFetch.ssn === undefined) {
+      const fetchPatient = async () => {
+        try {        
+          const { data: patientFromApi } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+          dispatch({ type: "GET_PATIENT", payload: patientFromApi });
+        } catch(error) {
+          console.log(error);
+        }
+      };
+      fetchPatient();
+    }
   });
 
   const patient = Object.values(patients).find(p => p.id === id);
@@ -29,13 +33,14 @@ const PatientPage: React.FC = () => {
         <h1>{patient.name}</h1>
         <p>ssn: {patient.ssn}</p>
         <p>occupation: {patient.occupation}</p>
+        <p>gender: {patient.gender}</p>
       </div>
     );
   } 
   else {
     return (
       <div>
-        <p>Not found</p>
+        <p>Patient not found</p>
       </div>
     );
   }
